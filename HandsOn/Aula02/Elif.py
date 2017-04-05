207e2e2725c61df7975c9fc797098ddcb4c477b8
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2017-04-05 19:29:14 -0300

    Meu primeiro commit

---
+++ HandsOn/Aula02/Elif.py
@@ -0,0 +1,14 @@
+nome = 'B'
+if nome:
+	print 'PRIMEIRO IF'
+	if nome == 'D':
+		print 'SEGUNDO IF'
+
+elif nome == 'A':
+	print 'PRIMEIRO ELIF'
+
+elif nome == 'C':
+	print 'SEGUNDO ELIF'
+
+else:
+	print 'PRIMEIRO ELSE'
---
+++ HandsOn/Aula02/Hab.py
@@ -0,0 +1,19 @@
+#IDADE
+#ALCOOLIZADA
+#HABILITACAO
+
+idade = int(raw_input('Qual a sua idade? '))
+alcoolizada = raw_input('Esta alcoolizada? (S/N): ')
+Habilitada = raw_input ('Possui habilitacao? (S/N): ')
+
+if idade >= 18 and Habilitada == 'S' and alcoolizada == 'N:'
+	print 'Pode seguir, meu jovem!'
+
+else:
+
+	print 'Preso em nome da lei'
+
+
+
+
+
